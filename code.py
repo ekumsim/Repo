b03bbc38269207634149dc4e6fa555b2fe2f02f7
ekumsim <90572562+ekumsim@users.noreply.github.com>
2022-01-18 00:07:18 +0530

    Update code.py

--- code.py
+++ code.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'demo for git project flask webpage$$$$ '
+    return 'demo for git project flask webpage$$ '
 
 app.run(host='0.0.0.0', port=8000)
