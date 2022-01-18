833d6bcc52dac8f2059bc7e5ae3b528313c7f224
Ketan Vishwakarma <ketan.vishwakarma@inventum.net>
2022-01-18 19:08:06 +0530

    #4 update return message

--- code.py
+++ code.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'demo for git project flask webpage***** '
+    return 'Hello Sweety!'
 
 app.run(host='0.0.0.0', port=8000)
