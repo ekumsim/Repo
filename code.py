e2f4e779e84bb617ecfd823cdca11705389b94e3
ekumsim <90572562+ekumsim@users.noreply.github.com>
2022-01-19 01:10:20 +0530

    Hello to aws

--- code.py
+++ code.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello Jenkin!'
+    return 'Hello from Jenkin to AWS!'
 
 app.run(host='0.0.0.0', port=8000)
